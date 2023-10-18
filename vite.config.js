722ffe2518f0eef17736678726a3053f68de43c3
AMUTUHEIRE MELLON <amutuheiremellon3@gmail.com>
2023-10-18 15:36:29 +0300

    Add dotenv

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+// File: ./vite.config
+
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    define: { },
+    plugins: [vue()],
+    server: { port: env.VITE_PORT }
+  }
 })
