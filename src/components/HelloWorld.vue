265c11e290e5163983e56981d1cd216ee1352ef0
AMUTUHEIRE MELLON <amutuheiremellon3@gmail.com>
2023-10-24 19:05:47 +0300

    Sending data to the database

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,16 +6,33 @@
   const server = import.meta.env.VITE_SERVER
   const port = import.meta.env.VITE_API_PORT ? ':' + import.meta.env.VITE_API_PORT : ''
 
-  const helloString = ref('...')
-  // File: ./src/components/HelloWorld.vue
+  const helloString = ref('...');
+  const newHelloString = ref('');
 
 onMounted(async () => {
   const {data} = await axios.get(`${server}${port}/api/hello`)
   helloString.value = data.helloString
 })
+async function submitForm (){
+  try{
+    const response = await axios.post(`${server}${port}/api/hello`, { newHelloString: newHelloString.value });
+    console.log('POST request successful:', response.data);
+    newHelloString.value = '';
+  } catch (error){
+    console.error('Error sending POST request:', error);
+  }
+};
 </script>
 
-<template>
-    <h1>{{ helloString }}</h1>
-</template>
 
+<template>
+  <div>
+    <h1>{{ helloString }}</h1>
+    <input
+      type="text"
+      id="helloinput"
+      v-model="newHelloString"
+    />
+    <button @click="submitForm">Submit</button>
+  </div>
+</template>
