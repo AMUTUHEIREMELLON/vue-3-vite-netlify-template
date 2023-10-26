17fff914ebea59b5c6f3ff896a948f15853a8116
Anne <anne.gofoit@gmail.com>
2023-10-26 17:11:44 +0200

    Fix intendation and line breaks

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,27 +1,26 @@
-// File: ./src/components/HelloWorld.vue
-
 <script setup>
   import { onMounted, ref } from 'vue'
   import axios from 'axios'
+
   const server = import.meta.env.VITE_SERVER
   const port = import.meta.env.VITE_API_PORT ? ':' + import.meta.env.VITE_API_PORT : ''
 
   const helloString = ref('...');
   const newHelloString = ref('');
 
-onMounted(async () => {
-  const {data} = await axios.get(`${server}${port}/api/hello`)
-  helloString.value = data.helloString
-})
-async function submitForm (){
-  try{
-    const response = await axios.post(`${server}${port}/api/hello`, { newHelloString: newHelloString.value });
-    console.log('POST request successful:', response.data);
-    newHelloString.value = '';
-  } catch (error){
-    console.error('Error sending POST request:', error);
-  }
-};
+  onMounted(async () => {
+    const {data} = await axios.get(`${server}${port}/api/hello`)
+    helloString.value = data.helloString
+  })
+
+  async function submitForm (){
+    try{
+      const response = await axios.post(`${server}${port}/api/hello`, { newHelloString: newHelloString.value });
+      newHelloString.value = '';
+    } catch (error){
+      console.error('Error sending POST request:', error);
+    }
+  };
 </script>
 
 
@@ -30,7 +29,7 @@ async function submitForm (){
     <h1>{{ helloString }}</h1>
     <input
       type="text"
-      id="helloinput"
+      id="hello-input"
       v-model="newHelloString"
     />
     <button @click="submitForm">Submit</button>
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,3 @@
-// File: ./vite.config
-
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
